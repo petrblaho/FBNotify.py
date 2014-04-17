2b13245990643b75921255e2484cc8f31c1146a4
Petr Blaho <petrblaho@gmail.com>
2014-04-17 16:16:13 +0200

    Get rid off counter, use set features, add offline
    This gets rid off usage of counter variable via use of set difference.
    Also adds offlined friends notification.

--- fbonline.py
+++ fbonline.py
@@ -2,10 +2,9 @@ import requests
 import json
 import subprocess
 import time
-ACCESS_TOKEN = "CAADbJ2R6rpEBAKUP0NUVO5TrSQWk6B1pth9wxRQRZArTcvYvTAFHvw1AFh02qSAN8HZBGtVauDvQZAg61BpGzC3zcvYAkZAxOlyKsY4evM8t5LQ0ZAoYMfJpDSLMZBy4Qig9cP5JYeqQeLvMnhZAzgW4cccnZBr84KPMjrvaBOgPO90Gs0SkNYg1EIMdXnQDnQoZD"
+ACCESS_TOKEN = "ENTER YOUR ACCESS TOKEN"
 friends_online = set()
-friends_online_old = set() 
-counter = 0
+DEBUG = False
 
 def get_friends_online():
     '''Returns friends who are online'''
@@ -14,18 +13,21 @@ def get_friends_online():
     payload = {'q' : query, 'access_token' : ACCESS_TOKEN }
     r = requests.get('https://graph.facebook.com/fql', params=payload)
     result = json.loads(r.text)
+    if DEBUG: print(result)
+    friends = set()
     for name in result['data']:
-	    friends_online.add(name['name'])
-    return friends_online
+	    friends.add(name['name'])
+    return friends
 
 while 1:
-    if counter % 30 == 0:
-        friends_online_old = []
-    if len(get_friends_online()) > 0:
-        for friend in friends_online:
-	        if not friend in friends_online_old:
-	            message = "%s is online" % friend
-	            subprocess.Popen(['notify-send', message])
-	            friends_online_old.append(friend)
+    last_friends_online = friends_online.copy()
+    friends_online = get_friends_online()
+    new_friends_online = friends_online - last_friends_online
+    new_friends_offline = last_friends_online - friends_online
+    for friend in new_friends_online:
+        message = "%s is online" % friend
+        subprocess.Popen(['notify-send', message])
+    for friend in new_friends_offline:
+        message = "%s is offline" % friend
+        subprocess.Popen(['notify-send', message])
     time.sleep(60)
-    counter += 1
